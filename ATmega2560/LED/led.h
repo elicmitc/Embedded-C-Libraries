798f98dffd9926d6c9acdafa817fdebf7560fd4c
Elijah Mitchell <elicmitc@iu.edu>
2024-11-22 16:09:24 -0500

    Adding documentation and LED structure

---
+++ ATmega2560/LED/led.h
@@ -0,0 +1,18 @@
+#ifndef LED_H
+#define LED_H
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+// FUNCTION: (r, g, b)
+// FUNCTION: (leds, r, g, b, pin) 
+void leds(u_int16_t led_count, u_int8_t g, u_int8_t r, u_int8_t b, );
+// convert Green to 8 bit array
+// convert Red to 8 bit array
+// convert Blue to 8 bit array
+
+
+
+
+#endif
